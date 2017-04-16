bb178d7f4980b6a418283ea85667a77bf1a336db
John Nduli <yohanaizraeli@gmail.com>
2017-04-16 21:06:24 +0300

    Refactored code to make code that displays characters a function

--- codes/led_array.cpp
+++ codes/led_array.cpp
@@ -1,27 +1,31 @@
 #include "leds.h"
 const int nos = 100;
+void displayCharacters(Leds * leds){
+    for (int i= 0; i<nos; i++ )
+        leds->drawCharacter(leds->H);
+    for (int i= 0; i<nos; i++ )
+        leds->drawCharacter(leds->A);
+    for (int i= 0; i<nos; i++ )
+        leds->drawCharacter(leds->P);
+    for (int i= 0; i<nos; i++ )
+        leds->drawCharacter(leds->Y);
+    for (int i= 0; i<nos; i++ )
+        leds->drawCharacter(leds->E);
+    for (int i= 0; i<nos; i++ )
+        leds->drawCharacter(leds->S);
+    for (int i= 0; i<nos; i++ )
+        leds->drawCharacter(leds->T);
+    for (int i= 0; i<nos; i++ )
+        leds->drawCharacter(leds->R);
+    for (int i= 0; i<nos; i++ )
+        leds->drawCharacter(leds->H);
+}
 int main ()
 {
-   Leds leds; 
-   //leds.lightAllLEDs();
-   while (1){
-       for (int i= 0; i<nos; i++ )
-           leds.drawCharacter(leds.H);
-       for (int i= 0; i<nos; i++ )
-           leds.drawCharacter(leds.A);
-       for (int i= 0; i<nos; i++ )
-           leds.drawCharacter(leds.P);
-       for (int i= 0; i<nos; i++ )
-           leds.drawCharacter(leds.Y);
-       for (int i= 0; i<nos; i++ )
-           leds.drawCharacter(leds.E);
-       for (int i= 0; i<nos; i++ )
-           leds.drawCharacter(leds.S);
-       for (int i= 0; i<nos; i++ )
-           leds.drawCharacter(leds.T);
-       for (int i= 0; i<nos; i++ )
-           leds.drawCharacter(leds.R);
-       for (int i= 0; i<nos; i++ )
-           leds.drawCharacter(leds.H);
-   }
+    Leds leds; 
+    //leds.lightAllLEDs();
+
+    while (1){
+        displayCharacters(&leds);
+    }
 }
